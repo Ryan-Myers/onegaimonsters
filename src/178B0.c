25b8e15d48a2a928cad537c2cf301fd63890db3d
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-21 15:48:50 -0400

    Match func_801106B0

--- src/178B0.c
+++ src/178B0.c
@@ -1,8 +1,4 @@
 #include "common.h"
-struct Unk80109CB0 {
-    u8 pad[0x2C];
-    s32 unk2C;
-};
 // Could be alCSPGetState
 s32 func_80109CB0(ALCSPlayer *seqp)
 {
---
+++ src/1E2B0.c
@@ -0,0 +1,5 @@
+extern volatile int D_801821A8;
+
+void func_801106B0(void) {
+    while (D_801821A8 != 0) {}
+}
