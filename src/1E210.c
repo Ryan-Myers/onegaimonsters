8d2451f7b361e443192816d62773837858d38f28
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-21 07:39:05 -0400

    Identify osSetIntMask

--- src/1E210.c
+++ src/1E210.c
@@ -1,14 +1,16 @@
 #include "common.h"
 
-int func_801106B0();                                  /* extern */
-int func_80114110(int);                             /* extern */
+typedef unsigned int OSIntMask;
+int func_801106B0();
+OSIntMask osSetIntMask(OSIntMask);
 extern int D_80157640;
+#define OS_IM_NONE      0x00000001
 
 void func_80110610(int arg0) {
-    int temp_v0;
+    OSIntMask temp_v0;
 
     func_801106B0();
-    temp_v0 = func_80114110(1);
+    temp_v0 = osSetIntMask(OS_IM_NONE);
     D_80157640 = arg0;
-    func_80114110(temp_v0);
+    osSetIntMask(temp_v0);
 }
