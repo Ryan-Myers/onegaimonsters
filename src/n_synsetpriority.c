b9f59b8d22f60236c11e6b44b3ae021b46dfaaf0
Ryan Myers <ryan.p.myers@gmail.com>
2025-07-25 21:57:27 -0400

    Add n_libaudio

--- src/n_synsetpriority.c
+++ src/n_synsetpriority.c
@@ -1,18 +1,5 @@
 #include "common.h"
-
-/*
- * Synthesis driver stuff
- */
-typedef struct N_ALVoice_s {
-    ALLink              node;
-    struct N_PVoice_s     *pvoice;
-    ALWaveTable         *table;
-    void                *clientPrivate;
-    s16                 state;
-    s16                 priority;
-    s16                 fxBus;
-    s16                 unityPitch;
-} N_ALVoice;
+#include "PR/n_libaudio.h"
 
 void n_alSynSetPriority(N_ALVoice *voice, s16 priority) {
     voice->priority = priority;
