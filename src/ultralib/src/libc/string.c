058fc98672a49b8cf95cf706f4285bdcb873a496
Ryan Myers <ryan.p.myers@gmail.com>
2025-08-19 15:00:44 -0400

    Match libc files

--- src/ultralib/src/libc/string.c
+++ src/ultralib/src/libc/string.c
@@ -1,7 +1,36 @@
-#include "common.h"
+#include "PR/ultratypes.h"
+#include "string.h"
 
-INCLUDE_ASM("asm/nonmatchings/ultralib/src/libc/string", strchr);
+// TODO: this comes from a header
+#ident "$Revision: 1.23 $"
 
-INCLUDE_ASM("asm/nonmatchings/ultralib/src/libc/string", strlen);
+char* strchr(const char* s, int c) {
+    const char ch = c;
+    while (*s != ch) {
+        if (*s == 0) {
+            return NULL;
+        }
+        s++;
+    }
+    return (char*)s;
+}
 
-INCLUDE_ASM("asm/nonmatchings/ultralib/src/libc/string", memcpy);
+size_t strlen(const char* s) {
+    const char* sc = s;
+    while (*sc != 0) {
+        sc++;
+    }
+    return sc - s;
+}
+
+void* memcpy(void* s1, const void* s2, size_t n) {
+    char* su1 = (char*)s1;
+    const char* su2 = (const char*)s2;
+    while (n > 0) {
+        *su1 = *su2;
+        su1++;
+        su2++;
+        n--;
+    }
+    return (void*)s1;
+}
